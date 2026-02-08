d65f96a283188a2f21047c0ddbf151e7afc7a752
Campsiyas <campsiyasicloud@gmail.com>
2026-02-08 15:02:56 -0600

    Create script.sh
    script principal para ssh y casa os
    Modo dhcp

---
+++ script.sh
@@ -0,0 +1,15 @@
+
+##update repos
+sudo apt update
+##instalar open ssh
+sudo apt install openssh-server
+##Reiniciar equipo
+#reboot
+##usar estos comandos si ssh no funciona
+sudo systemctl enable ssh
+sudo systemctl start ssh
+##Instalar Dependencias
+sudo apt install cifs-utils
+sudo apt install mergerfs
+##Instalar casa os
+wget -qO- https://get.casaos.io | sudo bash
