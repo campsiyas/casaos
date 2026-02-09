543c7355375932ea2b15d27e12d18a39b9fc322d
Campsiyas <campsiyasicloud@gmail.com>
2026-02-09 17:35:43 -0600

    Update wifion.sh
    -wifi Ubuntu server

--- wifion.sh
+++ wifion.sh
@@ -1,4 +1,9 @@
-echo "[connection]\nwifi.powersave = 2" > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
-sudo systemctl restart NetworkManager
-echo "HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
-sudo systemctl restart systemd-logind.service
+##Ubuntu 16
+#echo "[connection]\nwifi.powersave = 2" > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+#sudo systemctl restart NetworkManager
+#echo "HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
+#sudo systemctl restart systemd-logind.service
+##Ubuntu server 
+#echo "[connection]\nwifi.powersave = 2" > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+sudo echo "HandleLidSwitch=ignore" >> nano /etc/systemd/logind.conf
+sudo echo "HandleLidSwitchDocked=ignore" >> nano /etc/systemd/logind.conf
