e8649ecfd794716bc5df7bb4238e9dcec62ea307
Campsiyas <campsiyasicloud@gmail.com>
2026-02-09 17:36:06 -0600

    Update wifion.sh

--- wifion.sh
+++ wifion.sh
@@ -4,6 +4,5 @@
 #echo "HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
 #sudo systemctl restart systemd-logind.service
 ##Ubuntu server 
-#echo "[connection]\nwifi.powersave = 2" > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 sudo echo "HandleLidSwitch=ignore" >> nano /etc/systemd/logind.conf
 sudo echo "HandleLidSwitchDocked=ignore" >> nano /etc/systemd/logind.conf
